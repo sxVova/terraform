cd4e9c6e506ae9049e754def747c8364c75ac06e
sxVova <rasavo99@gmail.com>
2020-02-18 17:27:02 +0200

    Work with storage 1.0

--- terraform_ansible/main.tf
+++ terraform_ansible/main.tf
@@ -52,5 +52,23 @@ resource "google_compute_instance" "web-server" {
       private_key = file("~/.ssh/id_rsa")
     }
   }
-  metadata_startup_script = "sudo apt update && sudo apt install software-properties-common -y && sudo apt-add-repository --yes --update ppa:ansible/ansible && sudo apt install ansible -y && sudo chmod 600 /home/rasavo99/.ssh/id_rsa && git clone https://github.com/sxVova/ansible.git /home/rasavo99/ansible && cd /home/rasavo99/ansible/ && ansible-playbook gitlab-playbook.yml && ansible-playbook java-jenkins.yml && ansible-playbook ssl-with-redirect-playbook.yml"
+  provisioner "file" {
+    source      = "./gitlab"
+    destination = "./gitlab"
+    connection {
+      type        = "ssh"
+      user        = "rasavo99"
+      host        = google_compute_address.static.address
+      private_key = file("~/.ssh/id_rsa")
+    }
+  }
+  metadata_startup_script = <<SCRIPT
+  sudo apt update
+  sudo apt install software-properties-common -y
+  sudo apt-add-repository --yes --update ppa:ansible/ansible
+  sudo apt install ansible -y
+  sudo chmod 600 /home/rasavo99/.ssh/id_rsa
+  git clone https://github.com/sxVova/ansible.git /home/rasavo99/ansible
+  sudo mv gitlab /home/rasavo99/ansible/group_vars/
+SCRIPT
 }
